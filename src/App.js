0bb8b20375303721dcb57b1bdab1e685c1403408
dp2005dp <meprasad78@gmail.com>
2025-10-29 09:44:54 +0530

    route change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+import { Route, Switch, Redirect} from 'react-router-dom'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
 import Login from './components/Login'
@@ -8,7 +8,7 @@ import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
+
     <Switch>
       <Route exact path="/login" component={Login} />
       <ProtectedRoute exact path="/" component={Home} />
@@ -17,7 +17,7 @@ const App = () => (
       <Route exact path="/not-found" component={NotFound} />
       <Redirect to="/not-found" />
     </Switch>
-  </BrowserRouter>
+ 
 )
 
 export default App
